4115c8d0ed960d7dbf0e533aa1fe6c1a16032da4
Layavandana <ryali.laya@gmail.com>
2020-03-13 21:30:25 -0700

    files adding

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package com.example.company;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     EditText txtname,txtpassword;
     Button btn_start;
+    Context context;
    DatabaseReference reff;
    Member member;
     @Override
@@ -25,14 +27,20 @@ public class MainActivity extends AppCompatActivity {
         txtname=(EditText)findViewById(R.id.inputUserNmae);
         txtpassword=(EditText)findViewById(R.id.inputPwd) ;
         member=new Member();
-        reff= FirebaseDatabase.getInstance().child("Member");
+        reff= FirebaseDatabase.getInstance().getReference().child("Member");
         btn_start= (Button) findViewById(R.id.login);
         Toast.makeText(this,"sucess", Toast.LENGTH_LONG).show();
+
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent a = new Intent(MainActivity.this, SecondActivity.class);
+                Intent a = new Intent(MainActivity.this, ProductActivity.class);
                 startActivity(a);
+
+                member.setName(txtname.getText().toString().trim());
+                member.setPassword(txtpassword.getText().toString().trim());
+                reff.child("member10").setValue(member);
+                Toast.makeText(MainActivity.this, "Data inserted successfully",Toast.LENGTH_LONG).show();
             }
         });
     }
